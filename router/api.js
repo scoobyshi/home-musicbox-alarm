37c0f142ab4776836fc00606ca11b2d9fdeab018
scoobyshi <scott.justason@gmail.com>
2017-02-05 14:07:35 -0500

    Simplify server and routing

--- router/api.js
+++ router/api.js
@@ -10,4 +10,34 @@ routes.post('/control/:action', (req, res) => {
   playcontrol.playback(action, res);
 });
 
+// Retrieve playlists
+routes.get('/playlists', (req, res) => {
+  let playcontrol = req.player;
+
+  console.log("Requested All Playlists API");
+
+  playcontrol.playlists(res);
+});
+
+// Load playlist
+routes.post('/load', (req, res) => {
+  let playlistid = req.query.playlistid;
+  let playcontrol = req.player;
+
+  console.log("Requested Loading API for Playlist ID:", playlistid);
+
+  playcontrol.load(playlistid, res);
+});
+
+// Get current track
+routes.get('/track/:type', (req, res) => {
+  // Expecting current, next, prev
+  let type = req.params.type || '';
+  let playcontrol = req.player;
+
+  console.log("Requested Track that is: " + type);
+
+  playcontrol.track(type, res);
+});
+
 module.exports = routes;
--- server.js
+++ server.js
@@ -1,97 +1,30 @@
 const express = require('express');
 const app = express();
-app.set('port', (process.env.PORT || 4001));
+const http = require('http');
+const server = http.createServer(app);
+const routes = require('./router/api');
 
-var Mopidy = require('mopidy');
-var mopidy = new Mopidy({
-  webSocketUrl: "ws://localhost:6680/mopidy/ws/"
-});
+// Attach Websocket endpoints for real-time events (eg play-pause, current song)
+var Music = require('./lib/musicService');
+var player = new Music.musicService(server, '/music/ws');
 
-// Log all activity and interaction with Mopidy to console
-// mopidy.on(console.log.bind(console));
-
-mopidy.on("state:online", () => {
-  console.log("Music Service is Online");
-});
-
+// Temporarily enable CORS for Dev only
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
 
-app.post('/api/next', (req, res, next) => {
-  console.log("Recieved API Request for Next Song");
+// Inflate the request with the websocket music player 
+var addPlayer = function (req, res, next) {
+  req.player = player;
+  next();
+}
+app.use(addPlayer);
 
-  mopidy.playback.next().then( () => {
-    // Mopidy doesn't actually return a result yet
-    console.log("200 - Next Song Success");
-    res.status(200).json({ "message": "success" });
-  })
-  .catch( () => {
-    res.status(400).json({ "error": "unknown" });
-  })
-  .done();
+// Provide API endpoints to control playback
+app.use('/music/api', routes);
+
+server.listen(5001, () => {
+  console.log("Listening on port: ", server.address().port);
 });
-
-app.post('/api/load/:playlist_uri', (req, res, next) => {
-  var playlist_uri = req.params.playlist_uri;
-  console.log("Recieved API Request to Load and Play Tracks for ID: " + playlist_uri);
-
-  // Clear the playlist, this could be an option
-  mopidy.tracklist.clear();
-  // .add(tracks, position, uri)
-  mopidy.tracklist.add(null, null, playlist_uri).then( () => {
-    if (req.query.action == "play") {
-      mopidy.playback.play();
-    } else if (req.query.action == "pause") {
-      mopidy.playback.pause();
-    }
-    res.status(200).json({ "message": "success" });
-  })
-  .catch( () => {
-    res.status(400).json({ "error": "unknown" });
-  })
-  .done();
-});
-
-app.get('/api/playlists', (req, res, next) => {
-  console.log("Recieved API Request for Available Playlists");
-
-  mopidy.playlists.getPlaylists().then( (playlists) => {
-    if (playlists) {
-      res.json(playlists);
-    } else {
-      res.status(400).json({ "error": "no playlists available" });
-    }
-  })
-  .catch( () => {
-    res.status(400).json({ "error": "unknown" });
-  })
-  .done();
-});
-
-app.get('/api/track', (req, res, next) => {
-  console.log("Recieved API Request for Current Track");
-
-  mopidy.playback.getCurrentTrack().then( (track) => {
-    if (track) {
-      console.log("200 - Success: " + JSON.stringify(track));
-      res.json(track);
-    } else {
-      console.log("400 - No Track");
-      res.status(400).json({ "error": "no track available" });
-    }
-  })
-  .catch( () => {
-    console.log("400 - Unknown");
-    res.status(400).json({ "error": "unknown" });
-  })
-  .done();
-});
-
-app.listen(app.get('port'), () => {
-  console.log("Find the server at: localhost:" + app.get('port'));
-});
-
-
